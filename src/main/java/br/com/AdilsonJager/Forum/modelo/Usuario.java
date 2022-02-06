6a2aa73711aba8f16b593b4dce9e184a6aff6665
Adilson Jäger <adilsonmagalhaesjager@gmail.com>
2022-02-06 16:33:30 +0000

    Add lambook

--- src/main/java/br/com/AdilsonJager/Forum/modelo/Usuario.java
+++ src/main/java/br/com/AdilsonJager/Forum/modelo/Usuario.java
@@ -5,9 +5,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
 public class Usuario {
 
+
+	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
@@ -38,37 +45,4 @@ public class Usuario {
 			return false;
 		return true;
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
 }
